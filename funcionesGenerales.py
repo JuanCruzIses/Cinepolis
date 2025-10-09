28e30f06cc4f15251b05a708dcf38c79939994b6
JuanCruzIses <juancruzises@gmail.com>
2025-10-09 19:11:11 -0300

    Remuevo el uso de la libreria levenshtein por problemas de uso desde las computadoras de mis compañeros. Dejo funcional la busqueda con expresiones regulares

--- funcionesGenerales.py
+++ funcionesGenerales.py
@@ -1,12 +1,10 @@
 from db import listaPeliculas
-from Levenshtein import distance
 import re
 
 """
     Funcionamiento general:
     1.Se solicita al usuario que ingrese el nombre de la pelicula a buscar
     2.A partir de un patron de busqueda, se filtra la lista de peliculas buscando coincidencias
-    4.Si no se encuentran coincidencias, se aplica una busqueda por similitud usando la distancia de Levenshtein (compara la diferencia existente entre la pelicula a buscar y los titulos de la lista de peliculas)
     3.Si se encuentran coincidencias, se muestran en un listado numerado y a partir de la eleccion del usuario se retorna el diccionario de la pelicula elegida
     5.Si no se encuentran coincidencias, se informa al usuario y se retorna 0
 
@@ -23,11 +21,6 @@ def buscarPelicula():
     resultados = [p for p in listaPeliculas if patron.search(p["titulo"].lower())]
 
     if peliculaABuscar != "0":
-        if not resultados:
-            umbral = 4
-            resultados = [p for p in listaPeliculas if distance(peliculaABuscar, p["titulo"].lower()) <= umbral]
-        
-        
         if resultados:
             print("Resultados encontrados:")
             for i in range(len(resultados)):
@@ -39,8 +32,12 @@ def buscarPelicula():
             if numeroPeliculaElegida != 0:
                 seleccionada = resultados[numeroPeliculaElegida - 1]
                 return seleccionada
-        else:
-            print("No se encontraron coincidencias.")
-            return False
-    return "0"
+            else:
+                print("Operación cancelada.")
+                return "0"
+    else:
+        print("Operación cancelada.")
+        return "0"
+
+buscarPelicula()
 
